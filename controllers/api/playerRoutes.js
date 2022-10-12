b89380212b7044af478759613a39ccc4e1306371
Jeffery Patterson <jpatterson933@ucla.edu>
2022-10-11 23:41:36 -0700

    updated model with custom validation

--- controllers/api/playerRoutes.js
+++ controllers/api/playerRoutes.js
@@ -9,4 +9,19 @@ router.get('/', async (req, res) => {
     res.json(playerData);
 });
 
+router.post('/', async (req, res) => {
+    try {
+        // this will assign the player data to playerData
+        const playerData = await Players.create(req.body);
+        // when done correctly, it will post a status of 200 and show us the playerData in json format
+        res.status(200).json(playerData);
+    } catch (err) {
+        console.log(err)
+        // 400 status code means the server could not understand the request
+        res.status(400).json(err);
+    }
+});
+
+
+
 module.exports = router;
--- models/players.js
+++ models/players.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class Players extends Model {}
+class Players extends Model { }
 
 Players.init(
     {
@@ -16,26 +16,24 @@ Players.init(
             type: DataTypes.STRING,
             allowNull: false,
             unique: true,
-            min: 1,
-            max: 14,
+            validate: { len: [1, 13] }
         },
         type: {
             type: DataTypes.STRING,
             allowNull: false,
-            // this is a custome validator to make sure the person selects the drop down from the menu and only warrrior, archer or knight can be accepted
+            // CUSTOM VALIDATOR
             validate: {
-                isDefinedType(value) {
-                    switch (value) {
-                        case (value !== 'warrior'):
-                            throw new Error('This is not a correct player type!!');
-                        case (value !== 'archer'):
-                            throw new Error('This is not an acceptable player type!!');
-                        case (value !== 'knight'):
-                            throw new Error('This is not an the correct type of player to pick!!');
-                        default:
-                            throw new Error('You have not chosen a player type!');
-                    }
-                    
+                // type must be of a specific character type else it will throw an error
+                isDefinedType(typeOf) {
+                    if (typeOf === 'warrior') {
+                        return;
+                    } else if (typeOf === 'archer') {
+                        return;
+                    } else if (typeOf === 'knight') {
+                        return;
+                    } else
+                        throw new Error('This is not a correct player type!!');
+
                 }
             }
         },
