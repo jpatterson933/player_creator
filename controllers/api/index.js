db54ac37e9f65fcdbd8717d0c35cfa0c79b370bf
Jeffery Patterson <jpatterson933@ucla.edu>
2022-10-11 22:56:20 -0700

    started public javascript for player creation

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 // grab the api for our players
 const playerRoutes = require('./playerRoutes');
 
-// set router to url and which routes to use
+// set router to url and which routes to use - full route is /api/players
 router.use('/players', playerRoutes);
 
 module.exports = router;
---
+++ public/js/createPlayer.js
@@ -0,0 +1,22 @@
+const createPlayerForm = async (event) => {
+    event.preventDefault();
+    // need if statement that will only accept value between 1 and 13
+    const charName = document.querySelector('#char-name').value;
+
+    const charType = document.querySelector('#char-type').value;
+
+
+    if(charName > 13) {
+        console.log("character name too long")
+    } else if (charName < 1) {
+        console.log("character name not long enough")
+    }
+
+    
+
+    console.log(charName, charType)
+};
+
+document
+    .querySelector('#create-char-form')
+    .addEventListener('submit', createPlayerForm);
