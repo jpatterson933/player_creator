2900afde9d8142490930159c18135f3c8c543706
Jeffery Patterson <jpatterson933@ucla.edu>
2022-10-11 23:06:28 -0700

    updated player.js type with a custom validator that only accepts warrior, archer or knight as a type

--- models/players.js
+++ models/players.js
@@ -21,7 +21,23 @@ Players.init(
         },
         type: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            // this is a custome validator to make sure the person selects the drop down from the menu and only warrrior, archer or knight can be accepted
+            validate: {
+                isDefinedType(value) {
+                    switch (value) {
+                        case (value !== 'warrior'):
+                            throw new Error('This is not a correct player type!!');
+                        case (value !== 'archer'):
+                            throw new Error('This is not an acceptable player type!!');
+                        case (value !== 'knight'):
+                            throw new Error('This is not an the correct type of player to pick!!');
+                        default:
+                            throw new Error('You have not chosen a player type!');
+                    }
+                    
+                }
+            }
         },
     },
     {
